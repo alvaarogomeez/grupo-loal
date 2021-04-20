6ddf3ac8adf6f6628a7b46f297767e3737880d9b
alvaarogomeez <agg150503@gmail.com>
2021-04-21 00:36:46 +0200

    nav function

--- js/script.js
+++ js/script.js
@@ -4,7 +4,26 @@ document.addEventListener( 'DOMContentLoaded', function () {
 		new Splide( '.splide' ).mount();
 });
 
+document.addEventListener('scroll', ()=>{
+	var floatingBtnWrapper = document.querySelector('#floating-btn-wrapper');
+	if(scrollY>=window.innerHeight){
+		floatingBtnWrapper.classList.add('active');
+		console.log('puesto');
+	}else{
+		floatingBtnWrapper.classList.remove('active');
+		console.log('quitado');
+	}
+});
 function toggleForm() {
 	var mainForm = document.querySelector('#main-form');
+	var floatingBtnWrapper = document.querySelector('#floating-btn-wrapper');
+	if(floatingBtnWrapper.classList.contains('active')){
+		floatingBtnWrapper.classList.remove('active');
+		console.log('quitado');
+	}else if(!floatingBtnWrapper.classList.contains('active') && scrollY>window.innerHeight){
+		floatingBtnWrapper.classList.add('active');
+		console.log('puesto');
+	}
+	
 	mainForm.classList.toggle('active');
-}
+};
