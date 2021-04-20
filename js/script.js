512d5ad0c528d6ce9c586a5774dad079e4a69330
alvaarogomeez <agg150503@gmail.com>
2021-04-20 10:35:52 +0200

    phone check

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
 document.addEventListener( 'DOMContentLoaded', function () {
+
+		// Splide Slider
 		new Splide( '.splide' ).mount();
-	} );
+});
+
+function toggleForm() {
+	var mainForm = document.querySelector('#main-form');
+	mainForm.classList.toggle('active');
+}
